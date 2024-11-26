a4a83b84b9da561d36ea367453901f48a030f6b4
Léonce Aklin <leonce.aklin@zebrapig.com>
2024-11-26 19:30:42 +0100

    Initial Commit

---
+++ const.py
@@ -0,0 +1,10 @@
+"""Constants for the Condair integration."""
+
+DOMAIN = "condair"
+
+
+def setup(hass, config):
+    hass.states.set("hello_state.world", "Paulus")
+
+    # Return boolean to indicate that initialization was successful.
+    return True
